1940aa0a88c6cdaae567f9994878c2428ef8adf6
yuechen <yuechen@bu.edu>
2019-11-07 08:55:28 -0500

    add form from antd

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { Register } from './Register'
 export class Main extends React.Component {
     render() {
         return (
-            <div>Main</div>
+            <div>
+                < Register />
+            </div>
         );
     }
 }
