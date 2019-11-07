046e97bfd3341acc665c71a18f02978bc25f351c
yuechen <yuechen@bu.edu>
2019-11-07 08:40:06 -0500

    add main

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from '../assets/images/logo.svg';
-import { TopBar } from './TopBar'
-
+import { TopBar } from './TopBar';
+import { Main } from './Main';
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Main />
     </div>
   );
 }
